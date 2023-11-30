55802c5d97fb3d6dc928169852078392c555a8be
Mario <mario.vista001@studenti.uniparthenope.it>
2023-11-30 16:29:56 +0100

    rename

---
+++ Es.Versi/main.cpp
@@ -0,0 +1,29 @@
+/**
+ * @file main.cpp
+ * @author Mario Vista (mario.vista001@studenti.uniparthenope.it)
+ * @brief 
+ * @version 0.1
+ * @date 2023-11-30
+ * 
+ * @copyright Copyright (c) 2023
+ * 
+ */
+
+ /**
+ * un file Problema.txt contenente diverse istanze del
+ * problema (una per ogni riga)
+ * un file Soluzione.txt contenente una soluzione per ogni
+ * istanza di problema (una per ogni riga)
+ * verifichi se ognuna delle soluzione è corretta rispetto al
+ * problema e scriva l'esito su un file Esito.txt
+ */
+
+
+#include <iostream>
+
+
+int main()
+{
+    std::cout << "Esercizio rispetta i versi" << std::endl;
+    return 0;
+}
