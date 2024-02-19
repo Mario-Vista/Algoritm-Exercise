35d08e4f731d2a475d5c6606cbc25887a8f22b34
m.vista <mario.vista001@studenti.uniparthenope.it>
2024-02-19 10:33:03 +0100

    Node

---
+++ Code/StruttureDati/Node.h
@@ -0,0 +1,39 @@
+#ifndef NODE_H
+#define NODE_H
+
+template <class T>
+class Node
+{
+    private:
+        Node<T>* next;
+        T* value;
+
+    public:
+        Node<T>(Node<T> *next, T* info);
+        ~Node();
+
+        void setNext(Node<T> next){this->next=next;}
+        void setData(T info){this->data = info;}
+
+        Node<T>* getNext(){return this->next;}
+        T* getData(){return this->data;}
+};
+
+
+template <class T>
+Node<T>::Node(Node<T>* next, T* info)
+{
+    this->next = next;
+    this->data = info;
+}
+data
+template <class T>
+Node<T>::~Node()
+{
+    delete next;
+    delete obj;
+} 
+
+
+
+#endif //NODE_H
