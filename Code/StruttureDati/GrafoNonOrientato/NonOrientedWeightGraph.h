b98de4853b7872368c38ccfb4c9296cc47f27690
mvista <mario.vista001@studenti.uniparthenope.it>
2024-02-28 13:42:34 +0100

    update

--- Code/StruttureDati/GrafoNonOrientato/NonOrientedWeightGraph.h
+++ Code/StruttureDati/GrafoNonOrientato/NonOrientedWeightGraph.h
@@ -5,6 +5,7 @@
 #include "Node.h"
 #include <iostream>
 #include "Vertex.h"
+#include "../../Algorithm/Algorithm.h"
 #include <limits>
 #include <queue>
 
@@ -56,22 +57,169 @@ class NonOrientedWeightGraph
     int time;
     vector<vector<int>> w;      //matrice dei pesi
     list<Vertex<T>*> verticiEstratti;
-    list<Vertex<T>*> getListaAdj();
+    list<Vertex<T>*> getListaAdj(Vertex<T>*);
     priority_queue< Vertex<T>*, vector<Vertex<T>*>, confronta<T> > minQueue;    //usata per prim
-    void makeSet(vertice<T>*);
+    void makeSet(Vertex<T>*);
     void unionA(Vertex<T>*, Vertex<T>*);
     void link(Vertex<T>*, Vertex<T>*);
-    Vertice<T>* findSet(Vertex<T>*);
+    Vertex<T>* findSet(Vertex<T>*);
 
     public:
         NonOrientedWeightGraph(){
-            w.resize(VERTEX_NUMBER);
+            w.resize(VERTEX_NUMBER, vector<int>(VERTEX_NUMBER));
         }
 
-    
+        vector<Node<T>> getGraph(){return this->graph;}
+
+        void addVertice(Node<T> nodo){this->graph.push_back(nodo);}
+        void addEdge(int i, Vertex<T>* vertice){this->graph.at(i).append(vertice);}
+        void addW(int i, int j, int w){this->w.at(i).at(j) = w;}
+
+        friend ostream& operator<<(ostream& out, const NonOrientedWeightGraph<T>& obj)
+        {
+            for(auto i:obj.graph)
+                out << i << endl;
+            return out;
+        }
+
+        bool appartieneCoda(Vertex<T>*);
+
+        void dfs();
+        void dfsVisit();
+
+        void connectedComponents();
+        void viewCC();
+        void mstKruskal();
+        void mstPrim(Vertex<T>*);
+
+        void viewPesi()
+        {
+            for(auto i: w)
+            {
+                for(auto j:i)
+                    cout << j << " ";
+                    cout << endl;
+            }
+        }
+
+        int getW(int i, int j)
+        {
+            return w.at(i).at(j);
+        }  
 
 };
 
+template <class T>
+list<Vertex<T>*> NonOrientedWeightGraph<T> :: getListaAdj(Vertex<T>*source)
+{
+    for(auto i : graph)
+        if(i.getSource() == source)
+            return i.getAdjList();
+        return graph.at(0).getAdjList();
+
+}
+
+
+template <class T>
+Vertex<T>* NonOrientedWeightGraph<T> :: findSet(Vertex<T>* x)
+{
+    if(x->getLeader() != x)
+        x->setLeader(findSet(x->getLeader()));
+    return x->getLeader();
+}
+
+template <class T>
+void NonOrientedWeightGraph<T> :: makeSet(Vertex<T>* x)
+{
+    x->setLeader(x);
+    x->setRank(0);
+}
+
+
+template <class T>
+void NonOrientedWeightGraph<T> :: unionA(Vertex<T>*x, Vertex<T> *y)
+{
+    this->link(findSet(x), findSet(y));
+}
+
+template<class T>
+void NonOrientedWeightGraph<T> :: link(Vertex<T>* x, Vertex<T>* y)
+{
+    if(x->getRank() > y->getRank())
+        x->setLeader(y);
+    else{
+        y->setLeader(x);
+        if(x->getRank == y->getRank())
+            y->setRank(y->getRank()+1);
+    }
+}
+
+template <class T>
+void NonOrientedWeightGraph<T> :: viewCC()
+{
+    cout << "***Componenti Connesse***" << endl;
+    for(auto v:graph)
+        cout << *(v.getSource()) << " " << *(v.getSource()->getPredecessor()) << endl;
+}
+
+template <class T>
+void NonOrientedWeightGraph<T> :: mstKruskal()
+{
+    for(auto v : graph)
+        this->makeSet(v.getSouce());
+
+    vector<Edge<T>> pesi;
+    for(int i=0; i < VERTEX_NUMBER; i++)
+        for(int j = 0; j < VERTEX_NUMBER; j++)
+            if(w.at(i).at(j) != 0)
+                pesi.push_back(Edge<T>(graph.at(i).getSource(), graph.at(j).getSource(), w.at(i).at(j)));
+    Algorithm::quickSort<Edge<T>>(pesi.begin(), pesi.end()-1);
+
+    for(auto i : graph)
+    {
+        if(findSet(i.getA()) != findSet(i.getB()))
+            unionA(i.getA(), i.getB());
+    }
+}
+
+template <class T>
+void NonOrientedWeightGraph<T> :: mstPrim(Vertex<T>* root)
+{
+    for(auto i:graph)
+    {
+        i.getSource()->setPredecessor(nullptr);
+        i.getSource()->setKey(UINT16_MAX);
+    }
+    root->setKey(0);
+
+    for(auto i:graph)
+        minQueue.push(i.getSource());
+
+    while(!minQueue.empty())
+    {
+        auto u = minQueue.top();
+        verticiEstratti.push_back(u);
+        minQueue.pop();
+
+        auto adj = this->getListaAdj(u);
+
+        for(auto v:adj)
+            if(appartieneCoda(v) && getW(u->getID(), v->getID()) < v->getKey())
+                v->setPredecessor(u);
+    }
+}
+
+
+
+template <class T>
+bool NonOrientedWeightGraph<T> :: appartieneCoda(Vertex<T>*v)
+{
+    bool appartiene = true;
+    for( auto i : verticiEstratti)
+        if(i == v)
+            appartiene = false;
+    return appartiene;
+}
 
 
 #endif //NONORIENTEDWEIGHTGRAPH_H
--- Code/StruttureDati/GrafoNonOrientato/main.cpp
+++ Code/StruttureDati/GrafoNonOrientato/main.cpp
@@ -1,4 +1,5 @@
 #include "NonOrientedGraph.h"
+#include "NonOrientedWeightGraph.h"
 #include <string>
 #include "Vertex.h"
 #include <iostream>
@@ -8,7 +9,7 @@ using namespace std;
 
 int main(){
 
-    NonOrientedGraph<string> grafoNonOrientato;
+    // NonOrientedGraph<string> grafoNonOrientato;
 
     Vertex<string> a("A");
     Vertex<string> b("B");
@@ -21,141 +22,141 @@ int main(){
     Vertex<string> i("I");
     Vertex<string> j("J");
 
-    grafoNonOrientato.addVertex(Node<string>(&a));
-    grafoNonOrientato.addVertex(Node<string>(&b));
-    grafoNonOrientato.addVertex(Node<string>(&c));
-    grafoNonOrientato.addVertex(Node<string>(&d));
-    grafoNonOrientato.addVertex(Node<string>(&e));
-    grafoNonOrientato.addVertex(Node<string>(&f));
-    grafoNonOrientato.addVertex(Node<string>(&g));
-    grafoNonOrientato.addVertex(Node<string>(&h));
-    grafoNonOrientato.addVertex(Node<string>(&i));
-    grafoNonOrientato.addVertex(Node<string>(&j));
+    // grafoNonOrientato.addVertex(Node<string>(&a));
+    // grafoNonOrientato.addVertex(Node<string>(&b));
+    // grafoNonOrientato.addVertex(Node<string>(&c));
+    // grafoNonOrientato.addVertex(Node<string>(&d));
+    // grafoNonOrientato.addVertex(Node<string>(&e));
+    // grafoNonOrientato.addVertex(Node<string>(&f));
+    // grafoNonOrientato.addVertex(Node<string>(&g));
+    // grafoNonOrientato.addVertex(Node<string>(&h));
+    // grafoNonOrientato.addVertex(Node<string>(&i));
+    // grafoNonOrientato.addVertex(Node<string>(&j));
 
-    grafoNonOrientato.addEdge(0, &b);
-    grafoNonOrientato.addEdge(0, &c);
+    // grafoNonOrientato.addEdge(0, &b);
+    // grafoNonOrientato.addEdge(0, &c);
 
-    grafoNonOrientato.addEdge(1, &a);
-    grafoNonOrientato.addEdge(1, &c);
-    grafoNonOrientato.addEdge(1, &d);
+    // grafoNonOrientato.addEdge(1, &a);
+    // grafoNonOrientato.addEdge(1, &c);
+    // grafoNonOrientato.addEdge(1, &d);
 
-    grafoNonOrientato.addEdge(2, &a);
-    grafoNonOrientato.addEdge(2, &b);
+    // grafoNonOrientato.addEdge(2, &a);
+    // grafoNonOrientato.addEdge(2, &b);
 
-    grafoNonOrientato.addEdge(3, &b);
+    // grafoNonOrientato.addEdge(3, &b);
 
-    grafoNonOrientato.addEdge(4, &f);
-    grafoNonOrientato.addEdge(4, &g);
+    // grafoNonOrientato.addEdge(4, &f);
+    // grafoNonOrientato.addEdge(4, &g);
 
-    grafoNonOrientato.addEdge(5, &e);
+    // grafoNonOrientato.addEdge(5, &e);
     
-    grafoNonOrientato.addEdge(6, &e);
+    // grafoNonOrientato.addEdge(6, &e);
 
-    grafoNonOrientato.addEdge(7, &i);
+    // grafoNonOrientato.addEdge(7, &i);
 
-    grafoNonOrientato.addEdge(8, &h);
+    // grafoNonOrientato.addEdge(8, &h);
 
-    grafoNonOrientato.dfs();
+    // grafoNonOrientato.dfs();
 
-    cout << "***Grafo Non Orientato***" << endl;
-    cout << grafoNonOrientato;
-
-
-    // GrafoNonOrientatoPesato<string> grafoNonOrientatoPesato;
-
-    // grafoNonOrientatoPesato.addVertice(&a);
-    // grafoNonOrientatoPesato.addVertice(&b);
-    // grafoNonOrientatoPesato.addVertice(&c);
-    // grafoNonOrientatoPesato.addVertice(&d);
-    // grafoNonOrientatoPesato.addVertice(&e);
-    // grafoNonOrientatoPesato.addVertice(&f);
-    // grafoNonOrientatoPesato.addVertice(&g);
-    // grafoNonOrientatoPesato.addVertice(&h);
-    // grafoNonOrientatoPesato.addVertice(&i);
-
-    // grafoNonOrientatoPesato.addEdge(0, &b);
-    // grafoNonOrientatoPesato.addEdge(0, &h);
-
-    // grafoNonOrientatoPesato.addEdge(1, &a);
-    // grafoNonOrientatoPesato.addEdge(1, &c);
-    // grafoNonOrientatoPesato.addEdge(1, &h);
-
-    // grafoNonOrientatoPesato.addEdge(1, &h);
-    // grafoNonOrientatoPesato.addEdge(2, &b);
-    // grafoNonOrientatoPesato.addEdge(2, &d);
-    // grafoNonOrientatoPesato.addEdge(2, &f);
-    // grafoNonOrientatoPesato.addEdge(2, &i);
-
-    // grafoNonOrientatoPesato.addEdge(3, &c);
-    // grafoNonOrientatoPesato.addEdge(3, &e);
-    // grafoNonOrientatoPesato.addEdge(3, &f);
-
-    // grafoNonOrientatoPesato.addEdge(4, &d);
-    // grafoNonOrientatoPesato.addEdge(4, &f);
-
-    // grafoNonOrientatoPesato.addEdge(5, &c);
-    // grafoNonOrientatoPesato.addEdge(5, &d);
-    // grafoNonOrientatoPesato.addEdge(5, &e);
-    // grafoNonOrientatoPesato.addEdge(5, &f);
-
-    // grafoNonOrientatoPesato.addEdge(6, &f);
-    // grafoNonOrientatoPesato.addEdge(6, &h);
-    // grafoNonOrientatoPesato.addEdge(6, &i);
-
-    // grafoNonOrientatoPesato.addEdge(7, &a);
-    // grafoNonOrientatoPesato.addEdge(7, &b);
-    // grafoNonOrientatoPesato.addEdge(7, &g);
-    // grafoNonOrientatoPesato.addEdge(7, &i);
-
-    // grafoNonOrientatoPesato.addEdge(8, &c);
-    // grafoNonOrientatoPesato.addEdge(8, &g);
-    // grafoNonOrientatoPesato.addEdge(8, &h);
-
-    // // A-0 B-1 C-2 D-3 E-4 F-5 G-6 H-7 I-8
-    // grafoNonOrientatoPesato.addW(0,1,4);
-    // grafoNonOrientatoPesato.addW(0,7,8);
-
-    // grafoNonOrientatoPesato.addW(1,0,4);
-    // grafoNonOrientatoPesato.addW(1,2,8);
-    // grafoNonOrientatoPesato.addW(1,7,11);
-
-    // grafoNonOrientatoPesato.addW(2,1,8);
-    // grafoNonOrientatoPesato.addW(2,3,7);
-    // grafoNonOrientatoPesato.addW(2,5,4);
-    // grafoNonOrientatoPesato.addW(2,8,2);
-
-    // grafoNonOrientatoPesato.addW(3,2,7);
-    // grafoNonOrientatoPesato.addW(3,4,9);
-    // grafoNonOrientatoPesato.addW(3,5,14);
-
-    // grafoNonOrientatoPesato.addW(4,3,9);
-    // grafoNonOrientatoPesato.addW(4,5,10);
-
-    // grafoNonOrientatoPesato.addW(5,2,4);
-    // grafoNonOrientatoPesato.addW(5,3,14);
-    // grafoNonOrientatoPesato.addW(5,4,10);
-    // grafoNonOrientatoPesato.addW(5,6,2);
-
-    // // A-0 B-1 C-2 D-3 E-4 F-5 G-6 H-7 I-8
-    // grafoNonOrientatoPesato.addW(6,5,2);
-    // grafoNonOrientatoPesato.addW(6,7,1);
-    // grafoNonOrientatoPesato.addW(6,8,6);
-
-    // grafoNonOrientatoPesato.addW(7,0,8);
-    // grafoNonOrientatoPesato.addW(7,1,11);
-    // grafoNonOrientatoPesato.addW(7,6,1);
-    // grafoNonOrientatoPesato.addW(7,8,7);
-
-    // grafoNonOrientatoPesato.addW(8,2,2);
-    // grafoNonOrientatoPesato.addW(8,6,6);
-    // grafoNonOrientatoPesato.addW(8,7,7);
-
-    // cout << "w(h,b): " << grafoNonOrientatoPesato.getW(7,1) << endl; 
+    // cout << "***Grafo Non Orientato***" << endl;
+    // cout << grafoNonOrientato;
 
 
-    // grafoNonOrientatoPesato.viewPesi();
+    NonOrientedWeightGraph<string> grafoNonOrientatoPesato;
 
-    // grafoNonOrientatoPesato.mstPrim(&a);
+    grafoNonOrientatoPesato.addVertice(&a);
+    grafoNonOrientatoPesato.addVertice(&b);
+    grafoNonOrientatoPesato.addVertice(&c);
+    grafoNonOrientatoPesato.addVertice(&d);
+    grafoNonOrientatoPesato.addVertice(&e);
+    grafoNonOrientatoPesato.addVertice(&f);
+    grafoNonOrientatoPesato.addVertice(&g);
+    grafoNonOrientatoPesato.addVertice(&h);
+    grafoNonOrientatoPesato.addVertice(&i);
+
+    grafoNonOrientatoPesato.addEdge(0, &b);
+    grafoNonOrientatoPesato.addEdge(0, &h);
+
+    grafoNonOrientatoPesato.addEdge(1, &a);
+    grafoNonOrientatoPesato.addEdge(1, &c);
+    grafoNonOrientatoPesato.addEdge(1, &h);
+
+    grafoNonOrientatoPesato.addEdge(1, &h);
+    grafoNonOrientatoPesato.addEdge(2, &b);
+    grafoNonOrientatoPesato.addEdge(2, &d);
+    grafoNonOrientatoPesato.addEdge(2, &f);
+    grafoNonOrientatoPesato.addEdge(2, &i);
+
+    grafoNonOrientatoPesato.addEdge(3, &c);
+    grafoNonOrientatoPesato.addEdge(3, &e);
+    grafoNonOrientatoPesato.addEdge(3, &f);
+
+    grafoNonOrientatoPesato.addEdge(4, &d);
+    grafoNonOrientatoPesato.addEdge(4, &f);
+
+    grafoNonOrientatoPesato.addEdge(5, &c);
+    grafoNonOrientatoPesato.addEdge(5, &d);
+    grafoNonOrientatoPesato.addEdge(5, &e);
+    grafoNonOrientatoPesato.addEdge(5, &f);
+
+    grafoNonOrientatoPesato.addEdge(6, &f);
+    grafoNonOrientatoPesato.addEdge(6, &h);
+    grafoNonOrientatoPesato.addEdge(6, &i);
+
+    grafoNonOrientatoPesato.addEdge(7, &a);
+    grafoNonOrientatoPesato.addEdge(7, &b);
+    grafoNonOrientatoPesato.addEdge(7, &g);
+    grafoNonOrientatoPesato.addEdge(7, &i);
+
+    grafoNonOrientatoPesato.addEdge(8, &c);
+    grafoNonOrientatoPesato.addEdge(8, &g);
+    grafoNonOrientatoPesato.addEdge(8, &h);
+
+    // A-0 B-1 C-2 D-3 E-4 F-5 G-6 H-7 I-8
+    grafoNonOrientatoPesato.addW(0,1,4);
+    grafoNonOrientatoPesato.addW(0,7,8);
+
+    grafoNonOrientatoPesato.addW(1,0,4);
+    grafoNonOrientatoPesato.addW(1,2,8);
+    grafoNonOrientatoPesato.addW(1,7,11);
+
+    grafoNonOrientatoPesato.addW(2,1,8);
+    grafoNonOrientatoPesato.addW(2,3,7);
+    grafoNonOrientatoPesato.addW(2,5,4);
+    grafoNonOrientatoPesato.addW(2,8,2);
+
+    grafoNonOrientatoPesato.addW(3,2,7);
+    grafoNonOrientatoPesato.addW(3,4,9);
+    grafoNonOrientatoPesato.addW(3,5,14);
+
+    grafoNonOrientatoPesato.addW(4,3,9);
+    grafoNonOrientatoPesato.addW(4,5,10);
+
+    grafoNonOrientatoPesato.addW(5,2,4);
+    grafoNonOrientatoPesato.addW(5,3,14);
+    grafoNonOrientatoPesato.addW(5,4,10);
+    grafoNonOrientatoPesato.addW(5,6,2);
+
+    // A-0 B-1 C-2 D-3 E-4 F-5 G-6 H-7 I-8
+    grafoNonOrientatoPesato.addW(6,5,2);
+    grafoNonOrientatoPesato.addW(6,7,1);
+    grafoNonOrientatoPesato.addW(6,8,6);
+
+    grafoNonOrientatoPesato.addW(7,0,8);
+    grafoNonOrientatoPesato.addW(7,1,11);
+    grafoNonOrientatoPesato.addW(7,6,1);
+    grafoNonOrientatoPesato.addW(7,8,7);
+
+    grafoNonOrientatoPesato.addW(8,2,2);
+    grafoNonOrientatoPesato.addW(8,6,6);
+    grafoNonOrientatoPesato.addW(8,7,7);
+
+    cout << "w(h,b): " << grafoNonOrientatoPesato.getW(7,1) << endl; 
+
+
+    grafoNonOrientatoPesato.viewPesi();
+
+    grafoNonOrientatoPesato.mstPrim(&a);
 
     return 0;
 }
